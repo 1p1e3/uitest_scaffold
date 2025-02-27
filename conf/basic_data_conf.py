1f0a8217152c9652e0bc50235054e4c505915156
chris <2762851151@qq.com>
2025-02-27 22:42:36 +0800

    :tada: 基础数据配置

---
+++ conf/basic_data_conf.py
@@ -0,0 +1,9 @@
+"""
+基础数据项配置，例如环境地址，api，账号等，可以进一步拆分的更细
+Basic data item configuration, such as environment addresses, APIs, and accounts, can be further subdivided into more detailed categories.
+"""
+
+# 群消息机器人 api
+feishu_bot_api = ""
+dingding_bot_api = ""
+wx_work_bot_api = ""
