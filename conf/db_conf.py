2806593c5d593dcf731b7c07ad33e486b87831a6
chris <2762851151@qq.com>
2025-02-27 23:19:52 +0800

    :new: 数据库连接配置

---
+++ conf/db_conf.py
@@ -0,0 +1,45 @@
+"""
+数据库相关配置，如主机、端口，账号密码等
+"""
+
+"""mysql"""
+
+mysql_instance1 = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "<PASSWORD>",
+}
+
+mysql_instance2 = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "<PASSWORD>",
+}
+
+
+"""redis"""
+
+redis_instance1 = {
+    "host": "127.0.0.1",
+    "port": 6379,
+    "user": "root",
+    "password": "<PASSWORD>",
+    "db": 0,
+}
+
+
+"""mongodb"""
+mongodb_instance1 = {
+    "host": "127.0.0.1",
+    "port": 27017,
+    "user": "root",
+    "password": "<PASSWORD>",
+    "db": 0,
+}
+
+...
+
+
+
